0987b311fc03527ed7730e841b65fda5718456b8
HostettlerW <williamhostettler@outlook.com>
2025-12-08 00:08:03 -0500

    create new v1

--- app/src/pages/02_CreateNewRecipe.py
+++ app/src/pages/02_CreateNewRecipe.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -9,7 +10,33 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Create your new recipe, {st.session_state['first_name']}.")
+try:
+    uID = st.session_state['user_id']
+    rURL = 'http://web-api:4000/r/recipes/maxID'
+    user_response = requests.get(rURL)
+    userJSON = user_response.json() if user_response.status_code == 200 else []
+except:
+    userJSON = []
+    st.write('# ERROR! CANNOT GET USER DATA!')
+
+generatedID = str(int(userJSON[0]["MaxID"]) + 1)
+
+st.title("Create New Recipe")
+st.write("### New ID: " + generatedID)
+
+in1 = st.text_input("Name")
+in2 = st.text_input("Description")
+in3 = st.text_area(
+    "Recipe Steps:",
+    height=200,   # make the box taller
+    placeholder="Write your steps here..."
+)
+in4 = st.slider(
+    "Difficulty",   # Label
+    min_value=1,          # Start
+    max_value=5,          # End
+    value=3               # Default position
+)
 
 if st.button('Example button.', 
              type='primary',
