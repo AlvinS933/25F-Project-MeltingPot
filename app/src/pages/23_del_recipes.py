44b9c3019d81637bcb72cb406c87ed1186ea67e7
HostettlerW <williamhostettler@outlook.com>
2025-12-08 12:15:52 -0500

    delete test

--- app/src/pages/23_del_recipes.py
+++ app/src/pages/23_del_recipes.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -9,9 +10,14 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+st.title("Recipe Deletion Tool")
+st.write("(Be Careful. Deletion is Permanent!)")
 
-if st.button('Example button.', 
+num = st.number_input("Enter a Recipe ID", min_value=0, value=0, step=1)
+
+if st.button('DELETE.', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/30_About.py')
+    cURL = 'http://web-api:4000/r/recipes/' + str(num)
+    c_response = requests.delete(cURL)
+    st.switch_page('pages/00_Recipe_Creator_Home.py')
