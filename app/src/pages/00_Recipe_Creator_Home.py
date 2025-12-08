a329908b75902076b7493e19c38007ed6b323f54
HostettlerW <williamhostettler@outlook.com>
2025-12-07 20:18:38 -0500

    Some work on Creator Home

--- app/src/pages/00_Recipe_Creator_Home.py
+++ app/src/pages/00_Recipe_Creator_Home.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -9,9 +10,32 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+try:
+    uID = st.session_state['user_id']
+    rURL = 'http://web-api:4000/u/users/' + str(uID)
+    user_response = requests.get(rURL)
+    userJSON = user_response.json() if user_response.status_code == 200 else []
+except:
+    userJSON = []
 
-if st.button('Example button.', 
+st.title(f"Welcome to the Creator Menu, {st.session_state['first_name']}.")
+
+if st.button('My Recipies', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/30_About.py')
+
+if st.button('Create New Recipie', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/30_About.py')
+
+if st.button('My Collections', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/30_About.py')
+
+if st.button('Create New Collection', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/30_About.py')
