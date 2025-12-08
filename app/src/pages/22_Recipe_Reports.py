c7a6c44205026c18593828e1c7c586b232c4292c
HostettlerW <williamhostettler@outlook.com>
2025-12-08 11:57:42 -0500

    new page

---
+++ app/src/pages/22_Recipe_Reports.py
@@ -0,0 +1,22 @@
+import logging
+import requests
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+try:
+    admin_id = st.session_state['admin_id']
+    rURL = f"http://web-api:4000/u/admins/rr/{admin_id}"  
+    user_response = requests.get(rURL)
+    userJSON = user_response.json() if user_response.status_code == 200 else []
+except:
+    userJSON = []
+    st.write('# ERROR! CANNOT GET USER DATA!')
+
+st.title("Your Recipe Reports")
