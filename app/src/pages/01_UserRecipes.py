73a1586538df67b7753628061682c50103079487
HostettlerW <williamhostettler@outlook.com>
2025-12-07 22:53:04 -0500

    List user recipies start

--- app/src/pages/01_UserRecipes.py
+++ app/src/pages/01_UserRecipes.py
@@ -1,4 +1,5 @@
 import logging
+import requests
 logger = logging.getLogger(__name__)
 
 import streamlit as st
@@ -9,7 +10,18 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
+try:
+    uID = st.session_state['user_id']
+    st.write("trying to request " + rURL)
+    user_response = requests.get(rURL)
+    userJSON = user_response.json() if user_response.status_code == 200 else []
+except:
+    userJSON = []
+    st.write('# ERROR! CANNOT GET USER DATA!')
+
 st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+
+st.write(userJSON)
 
 if st.button('Example button.', 
              type='primary',
