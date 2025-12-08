1b5f4f30c20ad9dc572c56e0d6e466fbab5e6a1b
HostettlerW <williamhostettler@outlook.com>
2025-12-07 20:41:03 -0500

    SubSite files for Creator

---
+++ app/src/pages/04_CreateNewCollection.py
@@ -0,0 +1,17 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import streamlit as st
+from modules.nav import SideBarLinks
+
+st.set_page_config(layout = 'wide')
+
+# Show appropriate sidebar links for the role of the currently logged in user
+SideBarLinks()
+
+st.title(f"Welcome Political Strategist, {st.session_state['first_name']}.")
+
+if st.button('Example button.', 
+             type='primary',
+             use_container_width=True):
+  st.switch_page('pages/30_About.py')
