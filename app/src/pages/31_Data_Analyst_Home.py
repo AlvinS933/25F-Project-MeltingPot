c5012756c8f01dddd19281f41a802c3ff79fae77
JakeYohn <yohn.j@northeastern.edu>
2025-12-07 23:37:43 -0500

    dataanalyst

--- app/src/pages/31_Data_Analyst_Home.py
+++ app/src/pages/31_Data_Analyst_Home.py
@@ -7,7 +7,6 @@ from modules.nav import SideBarLinks
 
 st.set_page_config(layout='wide')
 
-
 SideBarLinks()
 
 try:
@@ -19,30 +18,24 @@ except:
     analystJSON = []
     st.write('# ERROR! CANNOT GET DATA ANALYST DATA!')
 
-st.title(f"Welcome to the Data Analyst Menu.")
-
-
 
 
 if st.button('Chef Rating Overview',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/10_ChefRatingOverview.py')
-
+    st.switch_page('pages/32_Chef_Rating_Overview.py')
 
 if st.button('Popular Recipe Categories',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/11_PopularCategories.py')
+    st.switch_page('pages/33_Popular_Categories.py')
 
-
-if st.button('Trend Graphs & Monthly Comparison',
+if st.button('Trend Graphs and Monthly Comparison',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/12_TrendGraphs.py')
-
+    st.switch_page('pages/34_Trend_Graphs.py')
 
 if st.button('User Rating Explorer',
              type='primary',
              use_container_width=True):
-    st.switch_page('pages/13_UserRatingExplorer.py')
+    st.switch_page('pages/35_User_Rating_Explorer.py')
